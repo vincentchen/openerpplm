5e5d56aa94424d2e3ed0f4acf3ecf8582a81ea73
Fabio Colognesi <fabio.colognesi@omniasolutions.eu>
2014-02-22 11:03:46 -0600

    Added Where Used BoM Structure to Product actions. Reviewed TreeStructure Bom View.

--- install/plm_extend_entities.py
+++ install/plm_extend_entities.py
@@ -19,9 +19,8 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-
+import types
 from osv import osv, fields
-
 
 class plm_document(osv.osv):
     _name = 'plm.document'
@@ -39,13 +38,41 @@ plm_document()
 class plm_component(osv.osv):
     _name = 'product.product'
     _inherit = 'product.product'
+    
+#     def _fathers_compute(self, cr, uid, ids, name, arg, context=None):
+#         """ Gets father bom.
+#         @param self: The object pointer
+#         @param cr: The current row, from the database cursor,
+#         @param uid: The current user ID for security checks
+#         @param ids: List of selected IDs
+#         @param name: Name of the field
+#         @param arg: User defined argument
+#         @param context: A standard dictionary for contextual values
+#         @return:  Dictionary of values
+#         """
+#         result = []
+#         if context is None:
+#             context = {}
+#         bom_obj = self.pool.get('mrp.bom')
+#         bom_id = context and context.get('active_id', False) or False
+# 
+#         for bom in self.browse(cr, uid, ids, context=context):
+#             if (bom.id == bom_id):
+#                 tmp_ids = bom_obj.search(cr, uid, [('bom_id','!=',False),('product_id','=',bom_id)])
+#                 bom_parents = bom_obj.browse(cr, uid, tmp_ids, context=context)
+#                 for bom_parent in bom_parents:
+#                     if not (bom_parent.bom_id.id in result):
+#                         result.append(bom_parent.bom_id.id)
+# 
+#         return result
+
     _columns = {
         	    'linkeddocuments':fields.many2many('plm.document', 'plm_component_document_rel','component_id','document_id', 'Linked Docs'),  
                 'tmp_material': fields.many2one('plm.material','Raw Material', required=False, change_default=True, help="Select raw material for current product"),
 #                'tmp_treatment': fields.many2one('plm.treatment','Thermal Treatment', required=False, change_default=True, help="Select thermal treatment for current product"),
-                'tmp_surface': fields.many2one('plm.finishing','Surface Finishing', required=False, change_default=True, help="Select surface finishing for current product")
-               }
- 
+                'tmp_surface': fields.many2one('plm.finishing','Surface Finishing', required=False, change_default=True, help="Select surface finishing for current product"),
+              }
+
     def on_change_tmpmater(self, cr, uid, ids, tmp_material=False):
         values={'engineering_material':''}
         if tmp_material:
@@ -117,12 +144,45 @@ class plm_relation(osv.osv):
 
         return result
 
+    def _father_compute(self, cr, uid, ids, name, arg, context=None):
+        """ Gets father bom.
+        @param self: The object pointer
+        @param cr: The current row, from the database cursor,
+        @param uid: The current user ID for security checks
+        @param ids: List of selected IDs
+        @param name: Name of the field
+        @param arg: User defined argument
+        @param context: A standard dictionary for contextual values
+        @return:  Dictionary of values
+        """
+        
+        result = {}
+        if context is None:
+            context = {}
+        bom_obj = self.pool.get('mrp.bom')
+        bom_lines = bom_obj.browse(cr, uid, ids, context=context)
+        for bom_line in bom_lines:
+            result[bom_line.id]=[]
+            if bom_line.bom_id.id:
+                if not (bom_line.bom_id.id in result[bom_line.id]):
+                    result[bom_line.id]+=[bom_line.bom_id.id]
+            else:
+                for thisId in ids:
+                    tmp_ids = bom_obj.search(cr, uid, [('bom_id','!=',False),('product_id','=',bom_line.product_id.id)])
+                    bom_parents = bom_obj.browse(cr, uid, tmp_ids, context=context)
+                    for bom_parent in bom_parents:
+                        if bom_parent.bom_id.id:
+                            if not(bom_parent.bom_id.id in result[bom_line.id]):
+                                result[bom_line.id]+=[bom_parent.bom_id.id]
+        return result
+
     _columns = {
                 'state': fields.related('product_id','state',type="char",relation="product.template",string="Status",store=False),
                 'engineering_revision': fields.related('product_id','engineering_revision',type="char",relation="product.template",string="Revision",store=False),
                 'description': fields.related('product_id','description',type="char",relation="product.template",string="Description",store=False),
                 'weight_net': fields.related('product_id','weight_net',type="float",relation="product.product",string="Weight Net",store=False),
                 'child_complete_ids': fields.function(_child_compute, relation='mrp.bom', method=True, string="BoM Hierarchy", type='many2many'),
+                'father_complete_ids': fields.function(_father_compute, relation='mrp.bom', method=True, string="BoM Hierarchy", type='many2many'),
                }
 
 plm_relation()
