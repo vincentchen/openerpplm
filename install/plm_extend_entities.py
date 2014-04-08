e0e45524b8073011155fe71eda52bb31b11593d6
Fabio Colognesi <fabio.colognesi@omniasolutions.eu>
2014-04-08 09:50:05 +0200

    Minor issue.

--- install/plm_extend_entities.py
+++ install/plm_extend_entities.py
@@ -19,7 +19,6 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-import types
 from osv import osv, fields
 
 class plm_document(osv.osv):
