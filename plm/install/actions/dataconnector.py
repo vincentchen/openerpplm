0bcb29c52c97973d3d7e2ca0287919312d55d0f2
Fabio Colognesi <fabio.colognesi@omniasolutions.eu>
2015-07-31 01:37:43 -0500

    Merged changes from 8.0

--- plm/install/actions/dataconnector.py
+++ plm/install/actions/dataconnector.py
@@ -20,7 +20,7 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-import os
+import os,sys
 import types
 import cPickle as pickle
 from datetime import datetime
@@ -181,6 +181,7 @@ class plm_component(osv.osv):
                 os.unlink(fileName)
         else:
             self.set_last_session
+#         return "2015-01-01 12:00:00"
         return lastDate.strftime('%Y-%m-%d %H:%M:%S')
 
     @property
@@ -264,8 +265,14 @@ class plm_component(osv.osv):
         return ""
 
     def TransferData(self, cr, uid, ids=False, context=None):
- 
-        operation=False
+         """
+            Exposed method to execute data transfer to other systems.
+        """ 
+#         Reset default encoding. to allow to work fine also as service.
+        reload(sys)
+        sys.setdefaultencoding('utf-8')
+#         Reset default encoding. to allow to work fine also as service.
+       operation=False
         reportStatus='Failed'
         updateDate=self.get_last_session
         logging.debug("[TransferData] Start : %s" %(str(updateDate)))
